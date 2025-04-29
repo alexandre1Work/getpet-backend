55a2f87836e27fd6360cd4aca2fd23f17612cbc4
alexandre1Work <alexandre.velosowork@gmail.com>
2025-04-29 20:02:57 -0300

    Feat: pet update

--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -17,5 +17,6 @@ router.get("/mypets", PetController.getAllMyPets)
 router.get("/myadoptions", verifyToken, PetController.getAllUserAdoptions)
 router.get('/:id', PetController.getPetById)
 router.delete('/:id', verifyToken, PetController.removePetById)
+router.patch('/:id', verifyToken, imageUpload.array("images"), PetController.updatePet)
 
 module.exports = router;
